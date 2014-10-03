2e17b57a49a1bf1ce45ea1670d2e700757823fb3
epsniff <esniff@gmail.com>
2014-10-03 13:55:37 -0700

    cleaning etcd docker up scripts

--- scripts/docker_run_etcd.sh
+++ scripts/docker_run_etcd.sh
@@ -6,10 +6,16 @@ echo sudo docker pull coreos/etcd
 echo 
 echo
 
-sudo docker stop $(sudo docker ps --no-trunc | grep coreos/etcd | awk '{print $1}')
 
-sudo docker run -d -p 8001:8001 -p 5001:5001 coreos/etcd -peer-addr ${PUBLIC_IP}:8001 -addr ${PUBLIC_IP}:5001 -name etcdnode1
-sudo docker run -d -p 8002:8002 -p 5002:5002 coreos/etcd -peer-addr ${PUBLIC_IP}:8002 -addr ${PUBLIC_IP}:5002 -name etcdnode2 -peers ${PUBLIC_IP}:8001,${PUBLIC_IP}:8002,${PUBLIC_IP}:8003
-sudo docker run -d -p 8003:8003 -p 5003:5003 coreos/etcd -peer-addr ${PUBLIC_IP}:8003 -addr ${PUBLIC_IP}:5003 -name etcdnode3 -peers ${PUBLIC_IP}:8001,${PUBLIC_IP}:8002,${PUBLIC_IP}:8003
+echo stopping existing etcd mTesetEtcdnode docker containers 
+sudo docker stop $(sudo docker ps --no-trunc | grep mTesetEtcdnode | awk '{print $1}')
+
+echo removing existing etcd mTesetEtcdnode docker containers 
+sudo docker rm $(sudo docker ps --no-trunc | grep mTesetEtcdnode | awk '{print $1}')
+
+echo starting new etcd mTesetEtcdnode docker containers 
+sudo docker run -d -p 8001:8001 -p 5001:5001 coreos/etcd -peer-addr ${PUBLIC_IP}:8001 -addr ${PUBLIC_IP}:5001 -name mTesetEtcdnode1
+sudo docker run -d -p 8002:8002 -p 5002:5002 coreos/etcd -peer-addr ${PUBLIC_IP}:8002 -addr ${PUBLIC_IP}:5002 -name mTesetEtcdnode2 -peers ${PUBLIC_IP}:8001,${PUBLIC_IP}:8002,${PUBLIC_IP}:8003
+sudo docker run -d -p 8003:8003 -p 5003:5003 coreos/etcd -peer-addr ${PUBLIC_IP}:8003 -addr ${PUBLIC_IP}:5003 -name mTesetEtcdnode3 -peers ${PUBLIC_IP}:8001,${PUBLIC_IP}:8002,${PUBLIC_IP}:8003
 
 
