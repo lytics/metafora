d2dea190992781d6914a838ac6ee857ce44ad263
epsniff <esniff@gmail.com>
2014-10-03 13:57:23 -0700

    Update docker_run_etcd.sh

--- scripts/docker_run_etcd.sh
+++ scripts/docker_run_etcd.sh
@@ -18,4 +18,5 @@ sudo docker run -d -p 8001:8001 -p 5001:5001 coreos/etcd -peer-addr ${PUBLIC_IP}
 sudo docker run -d -p 8002:8002 -p 5002:5002 coreos/etcd -peer-addr ${PUBLIC_IP}:8002 -addr ${PUBLIC_IP}:5002 -name mTesetEtcdnode2 -peers ${PUBLIC_IP}:8001,${PUBLIC_IP}:8002,${PUBLIC_IP}:8003
 sudo docker run -d -p 8003:8003 -p 5003:5003 coreos/etcd -peer-addr ${PUBLIC_IP}:8003 -addr ${PUBLIC_IP}:5003 -name mTesetEtcdnode3 -peers ${PUBLIC_IP}:8001,${PUBLIC_IP}:8002,${PUBLIC_IP}:8003
 
-
+echo list of running mTesetEtcdnode docker containers 
+sudo docker ps --no-trunc | grep mTesetEtcdnode
