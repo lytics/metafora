241baab53705cfdb9d2302005e8072fc39fd8625
Marek Dolgos <marek@lytics.io>
2014-10-03 18:36:09 -0700

    More work on the simple client to Metafora and Etcd.

--- m_etcd/const.go
+++ m_etcd/const.go
@@ -2,8 +2,8 @@ package m_etcd
 
 const (
 	TASKS_PATH   = "tasks"
-	COMMAND_PATH = "commands"
 	NODES_PATH   = "nodes"
+	COMMAND_PATH = "commands"
 	CLAIM_TTL    = 120 //seconds
 	TTL_FOREVER  = 0
 )
