89707bb7036cefbabb8cbca4359846d114ea3bb3
Michael Schurter <schmichael@lytics.io>
2015-04-09 11:35:30 -0700

    WIP Less state, no sync

--- statemachine/statemachine.go
+++ statemachine/statemachine.go
@@ -1,13 +1,15 @@
 package statemachine
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
-	"sync"
 	"time"
 
 	"github.com/lytics/metafora"
 )
+
+var ExceededErrorRate = errors.New("exceeded error rate")
 
 type StateCode int
 
@@ -17,13 +19,13 @@ const (
 	Completed                  // Terminal, not scheduled
 	Killed                     // Terminal, not scheduled
 	Failed                     // Terminal, not scheduled
-	Error                      // Scheduled, in error handling / retry logic
+	Fault                      // Scheduled, in error handling / retry logic
 	Paused                     // Scheduled, not running
 )
 
 func (s StateCode) Terminal() bool {
 	switch s {
-	case Runnable, Sleeping, Paused, Error:
+	case Runnable, Sleeping, Paused, Fault:
 		return false
 	case Completed, Killed, Failed:
 		return true
@@ -44,8 +46,8 @@ func (s StateCode) String() string {
 		return "killed"
 	case Failed:
 		return "failed"
-	case Error:
-		return "error"
+	case Fault:
+		return "fault"
 	case Paused:
 		return "paused"
 	default:
@@ -53,10 +55,53 @@ func (s StateCode) String() string {
 	}
 }
 
+type Err struct {
+	Time time.Time
+	Err  string
+}
+
+// ErrHandler functions should return Run, Sleep, or Fail messages depending on
+// the rate of errors.
+//
+// Either ErrHandler and/or StateStore should trim the error slice to keep it
+// from growing without bound.
+type ErrHandler func(taskID string, errs []Err) (Message, []Err)
+
+const (
+	DefaultErrLifetime = -4 * time.Hour
+	DefaultErrMax      = 8
+)
+
+// DefaultErrHandler returns a Fail message if 8 errors have occurred in 4
+// hours. Otherwise it enters the Sleep state for 10 minutes before trying
+// again.
+func DefaultErrHandler(_ string, errs []Err) (Message, []Err) {
+	recent := time.Now().Add(DefaultErrLifetime)
+	strikes := 0
+	for _, err := range errs {
+		if err.Time.After(recent) {
+			strikes++
+		}
+	}
+
+	if strikes >= DefaultErrMax {
+		// Return a new error to transition to Failed as well as the original
+		// errors to store what caused this failure.
+		return Message{Code: Error, Err: ExceededErrorRate}, errs
+	}
+	keeperrs := errs
+	if len(keeperrs) > DefaultErrMax {
+		keeperrs = keeperrs[len(keeperrs)-DefaultErrMax:]
+	}
+	return Message{Code: Sleep, Until: time.Now().Add(10 * time.Minute)}, keeperrs
+}
+
 type State struct {
-	Code  StateCode
-	Until time.Time
+	Code   StateCode
+	Until  time.Time
+	Errors []Err
 	//TODO Error related state?! Hm...
+	//Output interface{} //XXX A way to store progress?
 }
 
 func (s State) String() string {
@@ -72,22 +117,24 @@ type Message struct {
 	Code  MessageCode
 	Until time.Time // indicates time in Sleeping state by Sleep message
 	Err   error     // error associated with errMsgs
+	//Output interface{} //XXX A way to store progress?
 }
 
 type MessageCode int
 
 const (
-	// Public/explicit
 	Run MessageCode = iota
 	Sleep
 	Pause
 	Resume
-	Release
 	Kill
+	Error
+	Complete
+	Checkpoint //XXX or NOOP?
 
-	// Internal/implicit
-	errMsg
-	doneMsg
+	// Special event which triggers state machine to exit without transitioning
+	// between states.
+	Release
 )
 
 func (m MessageCode) String() string {
@@ -104,10 +151,12 @@ func (m MessageCode) String() string {
 		return "release"
 	case Kill:
 		return "kill"
-	case errMsg:
+	case Error:
 		return "error"
-	case doneMsg:
-		return "done"
+	case Complete:
+		return "complete"
+	case Checkpoint:
+		return "checkpoint"
 	default:
 		panic("unknown mesage code: " + strconv.Itoa(int(m)))
 	}
@@ -124,30 +173,32 @@ func (t Transition) String() string {
 }
 
 var (
+	// State Transition Table
 	Rules = []Transition{
 		// Runnable can transition to anything
-		{Event: Run, From: Runnable, To: Runnable},
+		{Event: Checkpoint, From: Runnable, To: Runnable},
 		{Event: Release, From: Runnable, To: Runnable},
 		{Event: Sleep, From: Runnable, To: Sleeping},
-		{Event: doneMsg, From: Runnable, To: Completed},
+		{Event: Complete, From: Runnable, To: Completed},
 		{Event: Kill, From: Runnable, To: Killed},
-		{Event: errMsg, From: Runnable, To: Error},
+		{Event: Error, From: Runnable, To: Fault},
 		{Event: Pause, From: Runnable, To: Paused},
 
 		// Sleeping can return to Runnable or be Killed/Paused
+		{Event: Checkpoint, From: Sleeping, To: Sleeping},
 		{Event: Sleep, From: Sleeping, To: Sleeping},
 		{Event: Run, From: Sleeping, To: Runnable},
 		{Event: Kill, From: Sleeping, To: Killed},
 		{Event: Pause, From: Sleeping, To: Paused},
 
-		// Error can return to Runnable or be put into any other state but Completed
-		{Event: Run, From: Error, To: Runnable},
-		{Event: Sleep, From: Error, To: Sleeping},
-		{Event: Pause, From: Error, To: Paused},
-		{Event: Kill, From: Error, To: Killed},
-		{Event: errMsg, From: Error, To: Failed},
+		// The error state transitions to either sleeping, failed, or released (to
+		// allow custom error handlers to workaround localitly related errors).
+		{Event: Sleep, From: Fault, To: Sleeping},
+		{Event: Error, From: Fault, To: Failed},
 
 		// Paused can return to Runnable, be put to Sleep, or Killed
+		{Event: Checkpoint, From: Paused, To: Paused},
+		{Event: Release, From: Paused, To: Paused},
 		{Event: Run, From: Paused, To: Runnable},
 		{Event: Sleep, From: Paused, To: Sleeping},
 		{Event: Kill, From: Paused, To: Killed},
@@ -157,21 +208,10 @@ var (
 	}
 )
 
-type StatefulHandler interface {
-	// Run a stateful handler
-	Run(sm StateMachine, taskID string) (done bool)
-
-	// Transitioned between states. This method is called and blocks state
-	// machine execution until it returns.
-	//
-	// StatefulHandler Run methods must exit after Transition is called with a
-	// Terminal state as it's impossible to transition out of Terminal states.
-	//
-	// Transition must not try to Apply a new state event.
-	//
-	//XXX Should this be blocking? Is letting it return an error a good idea?
-	Transition(t Transition) error
-}
+// Run a stateful handler until completion or a command is received. Handlers
+// can decide whether to return the command message directly or override it
+// with their own message.
+type StatefulHandler func(taskID string, commands <-chan Message) Message
 
 type StateStore interface {
 	// Load the persisted or initial state for a task. Errors will cause tasks to
@@ -183,137 +223,131 @@ type StateStore interface {
 	Store(taskID string, s State) error
 }
 
-// StateMachine interface provided to StatefulHandler.
-type StateMachine interface {
-	Apply(m Message) (State, error)
-	State() State
-}
-
 type stateMachine struct {
-	// initialized is closed once handler.Run has passed control over to the
-	// internal handler. It's necessary to prevent handler.Stop from trying to
-	// use an uninitialized state machine.
-	initialized chan bool
-
-	h   StatefulHandler
-	tid string
-	ss  StateStore
-
-	mu *sync.Mutex
-	s  State
+	h          StatefulHandler
+	ss         StateStore
+	cmd        chan Message
+	errHandler ErrHandler
 }
 
 // New handler that creates a state machine and exposes state transitions to
 // the given handler by calling its Transition method.
-func New(h StatefulHandler, ss StateStore) *stateMachine {
-	return &stateMachine{h: h, ss: ss, initialized: make(chan bool)}
+//
+// If ErrHandler==nil the default error handler will be used.
+func New(h StatefulHandler, ss StateStore, commands chan Message, e ErrHandler) *stateMachine {
+	if e == nil {
+		e = DefaultErrHandler
+	}
+	return &stateMachine{h: h, ss: ss, cmd: commands, errHandler: e}
 }
 
 // Run the state machine enabled handler. Loads the initial state and passes
 // control to the internal stateful handler.
 func (s *stateMachine) Run(taskID string) (done bool) {
-	s.tid = taskID
-	initial, err := s.ss.Load(taskID)
+	state, err := s.ss.Load(taskID)
 	if err != nil {
 		// A failure to load the state for a task is *fatal* - the task will be
 		// unscheduled and requires operator intervention to reschedule.
 		metafora.Errorf("task=%q could not load initial state. Marking done! Error: %v", taskID, err)
 		return true
 	}
-	s.mu = &sync.Mutex{}
-	s.s = initial
-	close(s.initialized)
+	if state.Code.Terminal() {
+		metafora.Warnf("task=%q in terminal state %s - exiting.", taskID, state.Code)
+		return true
+	}
 
 	// Main Run loop
 	done = false
 	for {
-		cur := s.State()
-		switch cur.Code {
+		var msg Message
+		var newstate State
+		metafora.Debugf("task=%q in state %s", taskID, state.Code)
+
+		// Execute state
+		switch state.Code {
 		case Runnable:
-			done = s.h.Run(s, taskID)
+			msg = run(s.h, taskID, s.cmd)
 		case Paused:
-			//TODO Wait on a new message
-			panic("not implemented")
+			msg = <-s.cmd
 		case Sleeping:
-			//TODO Wait on command
-			time.Sleep(cur.Until.Sub(time.Now()))
-		case Error:
-			//TODO check error history and possibly transition to Failed?
-			panic("not implemented")
+			dur := state.Until.Sub(time.Now())
+			metafora.Infof("task=%q sleeping for %s", taskID, dur)
+			timer := time.NewTimer(dur)
+			select {
+			case <-timer.C:
+				msg = Message{Code: Run}
+			case msg = <-s.cmd:
+				timer.Stop()
+			}
+		case Fault:
+			// Special case where we potentially trim the current state to keep
+			// errors from growing without bound.
+			msg, state.Errors = s.errHandler(taskID, state.Errors)
 		case Completed, Failed, Killed:
-			metafora.Infof("task=%q reached terminal state %s - exiting.", taskID, cur.Code)
-			return true
+			metafora.Infof("task=%q reached terminal state %s - exiting.", taskID, state.Code)
 		default:
-			panic("invalid state: " + cur.Code.String())
+			panic("invalid state: " + state.Code.String())
 		}
-		return done
+
+		// Apply message
+		newstate, ok := apply(state, msg)
+		if !ok {
+			metafora.Warnf("task=%q Invalid state transition=%q returned by task. Old state=%q", taskID, msg.Code, state.Code)
+			msg = Message{Code: Error, Err: err}
+			if newstate, ok = apply(state, msg); !ok {
+				metafora.Errorf("task=%q Unable to transition to error state! Exiting with state=%q", taskID, state.Code)
+				return state.Code.Terminal()
+			}
+		}
+
+		// Save state
+		if err := s.ss.Store(taskID, newstate); err != nil {
+			metafora.Errorf("task=%q Unable to persist state=%q. Unscheduling.", taskID, newstate.Code)
+			//FIXME Is this really the best thing to do?
+			return true
+		}
+
+		// Set next state and loop
+		state = newstate
+
+		// Exit and unschedule task on terminal state.
+		if state.Code.Terminal() {
+			return true
+		}
+
+		// Release messages indicate the task should exit but not unschedule.
+		if msg.Code == Release {
+			return false
+		}
 	}
 }
 
-func (s *stateMachine) run() bool {
+func run(f StatefulHandler, tid string, cmd <-chan Message) (m Message) {
 	defer func() {
 		if r := recover(); r != nil {
-			//TODO Transition to Error?
+			metafora.Errorf("task=%q Run method panic()d! Applying Error message. Panic: %v", tid, r)
+			m = Message{Code: Error, Err: fmt.Errorf("%v", r)} //FIXME This is a weird way of storing the panic
 		}
 	}()
-	return s.h.Run(s, s.tid)
+	m = f(tid, cmd)
+	return m
 }
 
-// Stop applies a Release message to the state machine to cause the handler to
-// exit but leave the task in a Runnable state.
+// Stop sends a Release message to the state machine.
 func (s *stateMachine) Stop() {
-	// Wait for the state machine to be initialized
-	<-s.initialized
-	if _, err := s.Apply(Message{Code: Release}); err != nil {
-		if it, ok := err.(InvalidTransition); ok {
-			metafora.Infof("task=%q couldn't be stopped: %s", s.tid, it)
-		} else {
-			metafora.Errorf("task=%q Unexpected error stopping: %v", s.tid, err)
-		}
-	}
+	//FIXME Could this block forever?!
+	s.cmd <- Message{Code: Release}
 }
 
-// Apply a message to cause a state transition. InvalidTransition is returned
-// with information about the current state since Apply can have concurrent
-// callers. A caller should inspect InvalidTransition and if appropriate retry
-// their state transition.
-func (s *stateMachine) Apply(m Message) (State, error) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	msg := m.Code
-	cur := s.s.Code
-
+// apply a message to cause a state transition. Returns false if the state
+// transition is invalid.
+func apply(cur State, m Message) (State, bool) {
 	//XXX Is a linear scan of all rules really the best option here?
 	for _, trans := range Rules {
-		if trans.Event == msg && trans.From == cur {
-			//XXX Should this really be done here?! And in a blocking fashion?!
-			//    Asynchronously notifying the handler via a goroutine that catches panic()s
-			if err := s.h.Transition(trans); err != nil {
-				metafora.Errorf("Error from transition %s handling: %v", trans, err)
-				return s.s, err
-			}
-			newstate := State{Code: trans.To, Until: m.Until}
-			if err := s.ss.Store(s.tid, newstate); err != nil {
-				metafora.Errorf("task=%q Error storing new state %s: %v", s.tid, newstate, err)
-				return s.s, err
-			}
-
-			// Now that the state has been stored, use it and continue
-			s.s = newstate
+		if trans.Event == m.Code && trans.From == cur.Code {
 			metafora.Debugf("Transitioned %s", trans)
-			return newstate, nil
+			return State{Code: trans.To, Until: m.Until}, true
 		}
 	}
-	return s.s, InvalidTransition{Message: msg, Current: cur}
-}
-
-// State returns the current state. Terminal states will never change.
-func (s *stateMachine) State() State {
-	// Wait for the state machine to be initialized
-	<-s.initialized
-	s.mu.Lock()
-	out := s.s
-	s.mu.Unlock()
-	return out
+	return cur, false
 }
--- statemachine/statemachine_test.go
+++ statemachine/statemachine_test.go
@@ -7,48 +7,58 @@ import (
 	"github.com/lytics/metafora/statemachine"
 )
 
-type testHandler struct {
-	done chan bool
-}
-
-func (h testHandler) Run(sm statemachine.StateMachine, taskID string) bool {
-	<-h.done
-	return true
-}
-
-func (h testHandler) Transition(t statemachine.Transition) error {
-	close(h.done)
-	return nil
+func testhandler(tid string, cmds <-chan statemachine.Message) statemachine.Message {
+	metafora.Debugf("Starting %s", tid)
+	m := <-cmds
+	metafora.Debugf("%s recvd %s", tid, m.Code)
+	return m
 }
 
 type testStore struct {
 	initial statemachine.State
+	out     chan<- statemachine.State
 }
 
 func (s testStore) Load(taskID string) (statemachine.State, error) {
+	s.out <- s.initial
 	return s.initial, nil
 }
-func (s testStore) Store(taskID string, _ statemachine.State) error {
+func (s testStore) Store(taskID string, newstate statemachine.State) error {
+	metafora.Debugf("%s storing %s", taskID, newstate.Code)
+	s.out <- newstate
 	return nil
 }
 
+//FIXME leaks goroutines
 func TestRules(t *testing.T) {
 	metafora.SetLogLevel(metafora.LogLevelDebug)
+
 	for i, trans := range statemachine.Rules {
+		metafora.Debugf("Trying %s", trans)
+		cmds := make(chan statemachine.Message)
+		store := make(chan statemachine.State)
+		ts := testStore{initial: statemachine.State{Code: trans.From}, out: store}
+
 		// Create a new statemachine that starts from the From state
-		sm := statemachine.New(testHandler{make(chan bool)}, testStore{statemachine.State{Code: trans.From}})
+		sm := statemachine.New(testhandler, ts, cmds, nil)
 		go sm.Run("test")
-		if sm.State().Code != trans.From {
-			t.Fatalf("%d Initial state %q not set. Found: %q", i, trans.From, sm.State().Code)
+		initial := <-store
+		if initial.Code != trans.From {
+			t.Fatalf("%d Initial state %q not set. Found: %q", i, trans.From, initial.Code)
 		}
 
-		// Apply the Event to transition to the To state
-		state, err := sm.Apply(statemachine.Message{Code: trans.Event})
-		if err != nil {
-			t.Fatalf("%d %s -- Unexpected error: %v", i, trans.Event, err)
+		// The Fault state transitions itself to either sleeping or failed
+		if trans.From != statemachine.Fault {
+			// Apply the Event to transition to the To state
+			cmds <- statemachine.Message{Code: trans.Event}
 		}
-		if state.Code != trans.To {
-			t.Fatalf("%d Expected %q but found %q", i, trans.To, state.Code)
+		newstate := <-store
+		if trans.From == statemachine.Fault && trans.To == statemachine.Failed {
+			//FIXME continue on as this transition relies on state this test doesn't exercise
+			continue
+		}
+		if newstate.Code != trans.To {
+			t.Fatalf("%d Expected %q but found %q", i, trans.To, newstate.Code)
 		}
 	}
 }
